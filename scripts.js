837795a28a1c77f895de8513be38a0600b618726
Roma-13 <romaasieshvili2007@gmail.com>
2023-11-15 22:58:19 +0400

    new changes

--- scripts.js
+++ scripts.js
@@ -1,16 +1,31 @@
-let counterValue = 0;
+    let counterValue = 0;
 
-function updateCounter(){
-    var counterElement = document.getElementById("counter");
-    counterElement.textContent = counterValue;
-}
+    function updateCounter(){
+        var counterElement = document.getElementById("counter");
+        counterElement.textContent = counterValue;
 
-function increaseCounter(){
-    counterValue += 1;
-    updateCounter()
-}
+        const maxLimit = 10;
+        if (counterValue > maxLimit){
+            alert("10 ზე მეტი რიცხვის მითითება შეუძლებელია")
 
-function decreaseCounter(){
-    counterValue -= 1;
-    updateCounter()
-}
+        }
+
+        const minLimit = 0;
+        if (counterValue < minLimit){
+            alert("0 ზე ნაკლები რიცხვის მითითება შეუძლებელია")
+        }
+    }
+
+    function increaseCounter(){
+        counterValue += 2;
+        updateCounter()
+        document.body.style.color = 'green';
+
+    }
+
+    function decreaseCounter(){
+        counterValue -= 2;
+        updateCounter()
+        document.body.style.color = 'red';
+
+    }
